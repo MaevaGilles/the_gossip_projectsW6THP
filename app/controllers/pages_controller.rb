4c78da29ede1ae357162f96431bd83dcc5977278
MaevaGilles <maevagilles-web@hotmail.fr>
2022-11-01 02:42:01 +0100

    new commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,22 @@
 class PagesController < ApplicationController
 
+    def home
+        @gossip = Gossip.all
+    
+    end
+
     def team
     
     end
+
+    def contact
+    
+    end
+
+    def bienvenue
+       @first_name = params[:first_name]
+    end
+
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+
+get 'home', to: 'pages#home', as: 'gossip'
 get 'team', to: 'pages#team'
+get 'contact', to: 'pages#contact'
+get 'bienvenue/:first_name', to: 'pages#bienvenue', as: "bienvenue"
 end
